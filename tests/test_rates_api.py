77663d9cfc9c02cdec91c93023f19cdedbbad25c
Douglas Treadwell <douglas.treadwell@gmail.com>
2016-09-26 17:04:06 -0700

    Clarifying self-documenting code in test_rates_api.py.

--- tests/test_rates_api.py
+++ tests/test_rates_api.py
@@ -2,8 +2,8 @@ from . import get_test_client, BaseTestCase, pallets
 
 
 class RatesApiTest(BaseTestCase):
-    origin_address = {'zip': '94107'}
-    destination_address = {'zip': '02108'}
+    minimal_origin_address = {'zip': '94107'}
+    minimal_destination_address = {'zip': '02108'}
 
     @classmethod
     def setUpClass(cls):
@@ -11,7 +11,7 @@ class RatesApiTest(BaseTestCase):
 
     def test_request_rate(self):
         self.client.rates.request({
-            'origin_address': self.origin_address,
-            'destination_address': self.destination_address,
+            'origin_address': self.minimal_origin_address,
+            'destination_address': self.minimal_destination_address,
             'items': pallets(4)
         })
